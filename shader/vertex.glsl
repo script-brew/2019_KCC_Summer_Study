e466a7f7fa51e3f08f3d04819132a549041a23e7
MaengSanHa <msh0117@kookmin.ac.kr>
2019-06-22 16:46:52 +0900

    temp save for CG

---
+++ shader/vertex.glsl
@@ -0,0 +1,13 @@
+#version 120                  // GLSL 1.20
+uniform mat4 u_PVM;
+
+attribute vec3 a_position;    // per-vertex position (per-vertex input)
+attribute vec3 a_color;       // per-vertex color (per-vertex input)
+
+varying   vec3 v_color;       // per-vertex color (per-vertex output)
+
+void main()
+{
+  gl_Position = vec4(a_position, 1.0f);
+  v_color = a_color;
+}

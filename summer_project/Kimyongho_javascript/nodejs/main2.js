cca35419aa7ee6cf847083f0056fb3efe4faa59a
yonghoya <39542937+yonghoya@users.noreply.github.com>
2019-07-28 01:36:18 +0900

    node.js를 이용한 자바스크립트 공부
    문법 및 실습(인터넷 공부)

---
+++ summer_project/Kimyongho_javascript/nodejs/main2.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query;
+    console.log(_url);
+    console.log(queryData.id);
+    if(_url == '/'){
+        _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    console.log(__dirname + _url);
+    response.end(queryData.id);
+
+});
+app.listen(3000);
